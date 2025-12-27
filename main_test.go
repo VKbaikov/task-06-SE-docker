d12d431163048e88c549b41a992595c02058cc6e
VKbaikov <vbajkov624@gmail.com>
2025-12-27 04:52:18 +0300

    Create main_test.go

---
+++ main_test.go
@@ -0,0 +1,15 @@
+package main
+
+import "testing"
+
+func TestAdd(t *testing.T) {
+    result := Add(2, 3)
+    if result != 5 {
+        t.Errorf("Add(2, 3) = %d; want 5", result)
+    }
+}
+
+func TestMainOutput(t *testing.T) {
+    // Пока пусто
+    main()
+}
